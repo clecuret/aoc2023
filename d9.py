eb25190bac0ca76165f7462ff3221f934b00de06
Cédric Lecuret <clecuret@gmail.com>
2023-12-09 14:16:14 +0100

    Add files via upload

---
+++ d9.py
@@ -0,0 +1,18 @@
+f=open('inputday9.txt')
+lines=f.readlines()
+f.close()
+total=0
+total2=0
+for l in lines:
+	alls=[]
+	data=[*map(int,l.split())]
+	curr=data
+	while sum(curr)!=0 or curr[0]!=0:
+		differences = [b - a for a, b in zip(curr, curr[1:])]
+		alls.append([curr[-1], curr[0]])
+		curr=differences
+	total += sum(d[0] for d in alls)
+	total2 += sum(d[1]* (-1)**(idx+2) for idx,d in enumerate(alls))
+
+print('Solution for Part 1 : %s' % total)
+print('Solution for Part 2 : %s' % total2)
